ede71f8a0645cad5331c34519ebe405cbc619a5b
Talha <talharrd5994@example.com>
2025-10-26 04:14:04 +0500

    l

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -114,7 +114,7 @@ const settings = {
     <>
     <div>
        <div>
-      <h1>Our Products</h1>
+      {/* <h1>Our Products</h1> */}
       {products.map(p => (
         <div key={p.id}>{p.name} - ${p.price}</div>
       ))}
